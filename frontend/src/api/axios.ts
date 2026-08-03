5a75d7dbbc94bab4d65a3f37e5e44ba434a2f36c
Mohit Raj <mohitraj191130@gmail.com>
2026-08-03 16:20:53 +0530

    Configure production API URL

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL:
-    import.meta.env.VITE_API_URL || "http://127.0.0.1:8000",
+    import.meta.env.VITE_API_URL ||
+    "http://127.0.0.1:8000",
+
   headers: {
     "Content-Type": "application/json",
   },
